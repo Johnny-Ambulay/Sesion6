526c4fb6d334ff333de99e7fc574f6c1e6ba28ff
Johnny-Ambulay <johnny.ambulay@upch.pe>
2024-09-14 09:30:52 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -6,9 +6,9 @@ from ramachandraw.utils import fetch_pdb
 from ramachandraw.utils import plot
 
 st.title("Generador de Diagrama de Ramachandran")
-st.text("Autor: Jesus Alvarado")
+st.text("Autor: Johnny Ambulay")
 
-pdb_id = st.text_input("Escribe el código PDB de 4 dígitos, por ejemplo: ", "3PL1")
+pdb_id = st.text_input("Escribe el código PDB de 4 dígitos, por ejemplo: ", "4RD")
 pdb_file = fetch_pdb(pdb_id)
 
 plt.figure()
